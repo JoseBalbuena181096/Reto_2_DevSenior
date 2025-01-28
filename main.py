bd96c14492c80ee26616920b716da7257b3789e1
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-01-27 21:45:16 -0600

    maquetación de clases

---
+++ main.py
@@ -0,0 +1,55 @@
+"""
+Sistema de gestión de la veterinaria con persistecia de datos a JSON
+Para el reto de DevSenior code Python
+
+Autor: José Ángel Balbuena Palma
+Fecha: 27/01/2025
+"""
+
+
+# ---------- Clase para la veterinaria ---------------
+class Veterinaria:
+    pass
+
+# ---------- Clase para la persona   ----------------
+class Persona:
+    pass
+
+# -------------- Herencias de la clase persona ----- 
+
+# --------- Clase para un cliente ------------------
+
+class Cliente(Persona):
+    pass
+
+
+class Veterinario(Persona):
+    pass
+
+# ------- Clases para el servicio y citas ----------
+
+class Servicio:
+    pass
+
+class Cita:
+    pass
+
+
+# ------- Clase para la mascota --------------------
+class Mascota:
+    pass
+
+
+# ------- Menu y validación de datos ---------------
+
+class Menu:
+    pass
+
+
+# ------- función principal y utilización -----------
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
